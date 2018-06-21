b58932763a62bf9ae928574a0e8d41ad9881f1c0
natalia <natalia.lucerot@gmail.com>
2018-06-21 15:34:02 -0500

    cambios malos de fetch

--- src/data.js
+++ src/data.js
@@ -1,78 +1,14 @@
-// window.computeUsersStats = (users, progress, courses) => {};
+ //window.computeUsersStats = (users, progress, courses) => {};
 // window.sortUsers = (users, orderBy, orderDirection) => {};
 // window.filterUsers = (users, search) => {};
 // window.processCohortData = (options) => {};
+/*
+const listaAlumnas = requiere('./users.json');
+const dataAlumnas = requiere('./progress.json');
 
+listaAlumnas.forEach((alumnas) => {
+    const idAlumna = alumnas.id;
+    console.log(alumna.name)
+});
 
-//fetch para users
-let users = [];
-const btn =  document.getElementById("dropdown2018");
-const container =  document.getElementById("contenedorCambiante");
-const userJson = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
-
-
-fetch(userJson)
-.then(response => response.json())
-.then(data => {
-    //renderUsers(data);
-    users = data;
-    console.log(users);
-})
-
-
-const renderUsers = data =>{
-    btn.addEventListener("click", () =>{
-        const render = data.forEach(element =>{
-            return container.innerHTML += (`<p>${element.id}</p>`)
-        })
-        return render;
-    })
-}
-
-
-//fetch para cohorts
-let cohorts = [];git 
-const cursos = document.getElementById("dropdownCurso");
-const containerDos= document.getElementById("contenedorCambiante");
-const cohortJson= "../data/cohorts/cohorts.json";
-
-fetch(cohortJson)
-.then(response => response.json())
-.then(data => {
-    renderCohorts(data);
-    cohorts = data;
-    console.log(data);
-})
-
-const renderCohorts = data =>{
-    cursos.addEventListener("click", () =>{
-        const render = data.forEach(element =>{
-            return containerDos.innerHTML += (`<p>${element.id}</p>`)
-        })
-        return render;
-    })
-}
-
-//fetch para progress
-let progress = {};
-const cursos = document.getElementById("dropdownCurso");
-const containerDos= document.getElementById("contenedorCambiante");
-const progressJson= "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
-
-fetch(cohortJson)
-.then(response => response.json())
-.then(data => {
-    renderCohorts(data);
-    progress =  data;
-    console.log(data);
-})
-
-const renderCohorts = data =>{
-    cursos.addEventListener("click", () =>{
-        const render = data.forEach(element =>{
-            return containerDos.innerHTML += (`<p>${element.intro}</p>`)
-        })
-        return render;
-    })
-}
-
+*/
