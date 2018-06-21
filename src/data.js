653a0f5f9b5a71aa87e9270697632f4e7fb89afc
natalia <natalia.lucerot@gmail.com>
2018-06-21 13:09:05 -0500

    modificacion de fetch

--- src/data.js
+++ src/data.js
@@ -4,7 +4,8 @@
 // window.processCohortData = (options) => {};
 
 
-
+//fetch para users
+let users = [];
 const btn =  document.getElementById("dropdown2018");
 const container =  document.getElementById("contenedorCambiante");
 const userJson = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
@@ -13,27 +14,33 @@ const userJson = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
 fetch(userJson)
 .then(response => response.json())
 .then(data => {
-    renderUsers(data);
-    console.log(data);
+    //renderUsers(data);
+    users = data;
+    console.log(users);
 })
 
+/*
 const renderUsers = data =>{
     btn.addEventListener("click", () =>{
         const render = data.forEach(element =>{
-            return container.innerHTML += (`<p>${element.name}</p>`)
+            return container.innerHTML += (`<p>${element.id}</p>`)
         })
         return render;
     })
 }
-
+*/
+/*
+//fetch para cohorts
+let cohorts = [];
 const cursos = document.getElementById("dropdownCurso");
 const containerDos= document.getElementById("contenedorCambiante");
-const cohortJson= "../data/cohorts.json";
+const cohortJson= "../data/cohorts/cohorts.json";
 
 fetch(cohortJson)
 .then(response => response.json())
 .then(data => {
     renderCohorts(data);
+    cohorts = data;
     console.log(data);
 })
 
@@ -45,3 +52,26 @@ const renderCohorts = data =>{
         return render;
     })
 }
+
+//fetch para progress
+let progress = {};
+const cursos = document.getElementById("dropdownCurso");
+const containerDos= document.getElementById("contenedorCambiante");
+const progressJson= "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+
+fetch(cohortJson)
+.then(response => response.json())
+.then(data => {
+    renderCohorts(data);
+    progress =  data;
+    console.log(data);
+})
+
+const renderCohorts = data =>{
+    cursos.addEventListener("click", () =>{
+        const render = data.forEach(element =>{
+            return containerDos.innerHTML += (`<p>${element.intro}</p>`)
+        })
+        return render;
+    })
+}*/
