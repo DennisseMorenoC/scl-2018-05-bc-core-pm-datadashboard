e460822c5e43036a4927d1209c858c32aea848d8
Dennisse Moreno C <deni.moreno11@gmail.com>
2018-06-22 15:52:23 -0400

    Comenzando funcion1

--- src/data.js
+++ src/data.js
@@ -1,37 +1,21 @@
 //import { promises } from "fs";
 
  window.computeUsersStats = (users, progress, courses) => {
-     for (i = 0; i < users.length; ++i){
-         let idAlumnas = users.map(function(id){
-             return idAlumnas;
-             console.log(idAlumnas);
-         });
-     }
+     
+    for (let i = 0; i < users.length; ++i){
+         let idAlumnas = users[i].id;
+         let progreso = progress[idAlumnas];
+             
+             
+         
+    };
+
  };
 // window.sortUsers = (users, orderBy, orderDirection) => {};
 // window.filterUsers = (users, search) => {};
 // window.processCohortData = (options) => {};
 
 
- Promise.all([
-    fetch ("../data/cohorts/lim-2018-03-pre-core-pw/users.json"),
-    fetch ("../data/cohorts/lim-2018-03-pre-core-pw/progress.json"),
-    fetch ("../data/cohorts.json")
- ]).then(
-     (responses) =>{
-     return Promise.all(responses.map((response)=>{
-         return response.json();
-     }));
- }
-).then((responseJsons)=>{
-    console.log(responseJsons);
-  // computeUsersStats(responseJsons[0],responseJsons[1],responseJsons[2]);
-}).catch(
-    (error) =>{
-        alert("Error al cargar los datos" + error);
-      console.log(error);
-    }
-);
-
+ 
 
 
