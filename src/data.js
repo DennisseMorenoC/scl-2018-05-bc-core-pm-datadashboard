4dc84c39ba6b99159304234f48a3c87cbc06eb60
natalia <natalia.lucerot@gmail.com>
2018-06-22 16:01:27 -0500

    modificacion de fetch

--- src/data.js
+++ src/data.js
@@ -1,47 +1,19 @@
  window.computeUsersStats = (users, progress, courses) => {
-     for (i = 0; i < users.length; ++i){
-         let idAlumnas = users.map(function(id){
-             return idAlumnas;
-             console.log(idAlumnas);
-         });
+     //creamos for para recorrer users
+     for (let i= 0; users.length; ++i){
+         //creamos variable users para poder obtener el id de cada alumna
+         let idAlumnas = users[i].id;
+         let progreso = progress[idAlumnas];
+         //creamos una segunda variable para 
+
      }
- };
+ }
+
+   
 // window.sortUsers = (users, orderBy, orderDirection) => {};
 // window.filterUsers = (users, search) => {};
 // window.processCohortData = (options) => {};
 
 
- Promise.all([
-    fetch ("../data/cohorts/lim-2018-03-pre-core-pw/users.json"),
-    fetch ("../data/cohorts/lim-2018-03-pre-core-pw/progress.json"),
-    fetch ("../data/cohorts.json")
- ]).then(
-     (responses) =>{
-     return Promise.all(responses.map((response)=>{
-         return response.json();
-     }));
- }
-).then((responseJsons)=>{
-    console.log(responseJsons);
-  // computeUsersStats(responseJsons[0],responseJsons[1],responseJsons[2]);
-}).catch(
-    (error) =>{
-        alert("Error al cargar los datos" + error);
-      console.log(error);
-    }
-);
-
-const users = response [0]
-const progress = response [1]
-const cohorts = response[2]
-
-let result = Object.keys(obj).map(function(key) {
-    return [progress(key), obj[key]];
-  });
-  
-  console.log(result);
-
-
-
-
+ 
 
